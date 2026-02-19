37005189a69f740309f0b97480fb04ee4fb2837f
Blake Bennett <144171214+blakebennett29@users.noreply.github.com>
2026-02-18 19:35:24 -0700

    Fix matrix slicing for A and B in exercise6.py

--- exercise6.py
+++ exercise6.py
@@ -2,7 +2,7 @@ from matrix_io import read_matrix
 import numpy as np
 
 mat = read_matrix("exercise6.csv")
-A = mat[::][0,2]
-B = mat[::][3,4]
+A = mat[::, :3]
+B = mat[::, 3:]
 sol = np.linalg.solve(A, B)
 print(sol)
